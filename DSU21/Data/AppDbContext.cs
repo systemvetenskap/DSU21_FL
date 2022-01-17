e7aa1b843189dfc348a5aa949b97d8c855011496
Erik Öberg <erik.oberg@miun.se>
2022-01-17 15:13:50 +0100

    Add project files.

---
+++ DSU21/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DSU21.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSU21.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Pirate> Pirates { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)  {  }
+    }
+}
---
+++ DSU21/Models/Poco/Pirate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DSU21.Models
+{
+    public class Pirate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+    }
+}
